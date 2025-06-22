daebf74bcfb6408a4b8827bc2a3b0ce09b72036b
Luukvdpoel <122754314+Luukvdpoel@users.noreply.github.com>
2025-06-22 23:30:13 +0200

    WIP: broken dev servers after tailwind + prisma install

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
+    'tailwindcss': {},           // ✅ Correct way to include Tailwind
+    'autoprefixer': {},
   },
-};
+}
