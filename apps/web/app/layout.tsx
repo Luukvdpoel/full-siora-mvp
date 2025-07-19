6c190d789ab3f77e00722017495f640e6064e216
Luukvdpoel <122754314+Luukvdpoel@users.noreply.github.com>
2025-07-20 00:35:49 +0200

    Cleanup unused assets and improve theming

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,15 @@
 "use client";
-import './globals.css';
-import type { ReactNode } from 'react';
-import { SessionProvider } from 'next-auth/react';
-import { BrandUserProvider } from '../lib/brandUser';
-import TrpcProvider from './trpcProvider';
-import { PageTransition, Nav, NavLink } from 'shared-ui';
-import * as React from 'react'
+import "./globals.css";
+import type { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+import { BrandUserProvider } from "../lib/brandUser";
+import TrpcProvider from "./trpcProvider";
+import { PageTransition, Nav, NavLink } from "shared-ui";
+import { ThemeProvider } from "./providers";
+import { Inter } from "next/font/google";
+import * as React from "react";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const navLinks: NavLink[] = [
   { href: '/dashboard', label: 'Dashboard' },
@@ -16,18 +20,22 @@ const navLinks: NavLink[] = [
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className="dark"> {/* for testing */}
-      <body className="bg-white text-black dark:bg-Siora-dark dark:text-white font-sans antialiased min-h-screen">
-        <SessionProvider>
-          <BrandUserProvider>
-            <TrpcProvider>
-              <main className="max-w-7xl mx-auto px-6 sm:px-8 py-10">
-                <Nav links={navLinks} />
-                <PageTransition>{children}</PageTransition>
-              </main>
-            </TrpcProvider>
-          </BrandUserProvider>
-        </SessionProvider>
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-white text-black dark:bg-Siora-dark dark:text-white font-sans antialiased min-h-screen`}
+      >
+        <ThemeProvider>
+          <SessionProvider>
+            <BrandUserProvider>
+              <TrpcProvider>
+                <main className="max-w-7xl mx-auto px-6 sm:px-8 py-10">
+                  <Nav links={navLinks} />
+                  <PageTransition>{children}</PageTransition>
+                </main>
+              </TrpcProvider>
+            </BrandUserProvider>
+          </SessionProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
