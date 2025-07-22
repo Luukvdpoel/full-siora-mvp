fd4043ff287e5e1334a31ed0f751a5f588ded806
Luukvdpoel <122754314+Luukvdpoel@users.noreply.github.com>
2025-07-22 17:26:21 +0200

    fix: ensure use client directive at top

--- apps/brand/app/layout.tsx
+++ apps/brand/app/layout.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 "use client";
+import React from 'react';
 import "./globals.css";
 import type { ReactNode } from "react";
 import { Nav, NavLink, PageTransition, ThemeToggle } from "shared-ui";
